6ee9dade0674362de916ab3d36bc5edf886a3ed9
guillermo._._ <luisgui.pizana@gmail.com>
2024-05-08 10:31:52 -0600

    Conv generator test.

--- src/model.py
+++ src/model.py
@@ -17,20 +17,35 @@ class Maxout(nn.Module):
         return x
 
 class Generator(nn.Module):
-    def __init__(self, config):
+    def __init__(self):
         super(Generator, self).__init__()
-        self.config = config
-        self.model = nn.Sequential(
-            nn.Linear(config["latent_size"], config["hidden_1"]), 
-            nn.ReLU(),
-            nn.Linear(config["hidden_1"], config["hidden_2"]),
-            nn.ReLU(),
-            nn.Linear(config["hidden_2"], 784), #The output of 784 is because the MNIST dataset has 784 features.
-            nn.Tanh()
-        )
+        # Start from a latent vector of a certain size, e.g., 100
+        self.latent_size = 100
+        
+        # First fully connected layer
+        self.fc = nn.Linear(self.latent_size, 7*7*128)  # Upscale to a 7x7 image with 128 channels
+        
+        # Transpose convolutions
+        self.conv_trans1 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)  # Output size: 14x14
+        self.batch_norm1 = nn.BatchNorm2d(64)
+        self.conv_trans2 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)  # Output size: 28x28
+        self.batch_norm2 = nn.BatchNorm2d(32)
+        
+        # Final layer to produce output image
+        self.conv_final = nn.Conv2d(32, 1, kernel_size=3, stride=1, padding=1)  # Output channel: 1 for grayscale image
+        
+        # Activation functions
+        self.relu = nn.ReLU(True)
+        self.tanh = nn.Tanh()  # Tanh to output values between -1 and 1
 
     def forward(self, x):
-        return self.model(x)
+        # Input x is the latent vector
+        x = self.fc(x)
+        x = x.view(-1, 128, 7, 7)  # Reshape to a 7x7 image with 128 channels
+        x = self.relu(self.batch_norm1(self.conv_trans1(x)))
+        x = self.relu(self.batch_norm2(self.conv_trans2(x)))
+        x = self.tanh(self.conv_final(x))
+        return x
 
 class Discriminator(nn.Module):
     def __init__(self, config):
